93c6d7cf061ba6d0b659eafc7550adc1ea51c137
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-03-02 00:42:58 +0300

    feat: add min, max and avg functions implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min;
+    if (array === undefined || array.length == 0)
+        return 0;
+    else {
+        min = array[0];
+        array.forEach(elem => {
+            if (elem < min) min = elem;
+        })
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let max;
+    if (array === undefined || array.length == 0)
+        return 0;
+    else {
+        max = array[0];
+        array.forEach(elem => {
+            if (elem > max) max = elem;
+        })
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let avg;
+    if (array === undefined || array.length == 0)
+        return 0;
+    else {
+        let sum = 0;
+        array.forEach(elem => {
+            sum += elem;
+        })
+        avg = sum / array.length;
+    }
+    return avg;
 }
